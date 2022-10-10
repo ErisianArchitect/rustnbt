34f206537e179a02954662877614a5e35d14569a
SevenSolaris <derekforgione@gmail.com>
2022-10-10 09:33:23 -0700

    Removed test code.

--- src/io.rs
+++ src/io.rs
@@ -12,15 +12,3 @@ pub trait NBTRead {
 pub trait NBTWrite {
     fn nbt_write(&self, writer: &mut _) -> Result<(), Error>;
 }
-
-impl<R: Read> NBTRead for i8 {
-    fn nbt_read(reader: &mut R) -> Result<Self, Error> {
-        let mut buf: &[u8] = &[0u8; 1];
-        reader.read_exact(&mut buf)?;
-        Ok(i8::from(buf[0]))
-    }
-}
-
-fn testit() {
-    i8::nbt_read(reader)
-}
