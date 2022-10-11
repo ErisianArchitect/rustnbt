fc3fb940ff8f5cb3027a9f5d1d1234e65e749a84
SevenSolaris <derekforgione@gmail.com>
2022-10-11 06:31:39 -0700

    Moved macro invocation into table.rs

--- src/tag.rs
+++ src/tag.rs
@@ -1,5 +1,6 @@
 // https://wiki.vg/NBT
 
+use crate::io::*;
 use indexmap::IndexMap;
 
 pub type Map = IndexMap<String, Tag>;
@@ -29,6 +30,27 @@ macro_rules! tag_info_table {
             pub fn id(&self) -> TagID {
                 match self {
                     $(Tag::$title{..} => TagID::$title,)+
+                }
+            }
+            pub fn size_in_bytes(&self) -> usize {
+                macro_rules! size_arm_match {
+                    ($tag_title:ident $item_ident:ident $_:ty) => {
+                        Tag::$tag_title($item_ident)
+                    };
+                    ($tag_title:ident) => {
+                        Tag::$tag_title
+                    };
+                }
+                macro_rules! size_arm_result {
+                    ($tag_title:ident $item_ident:ident $_:ty) => {
+                        $item_ident.size_in_bytes()
+                    };
+                    ($tag_title:ident) => {
+                        0
+                    };
+                }
+                match self {
+                    $(size_arm_match!($title $(item $type_)?) => size_arm_result!($title $(item $type_)?),)+
                 }
             }
         }
@@ -91,36 +113,7 @@ macro_rules! tag_info_table {
     };
 }
 
-//  id |title              |optional type_
-tag_info_table! {
-    00  End
-    01  Byte                i8
-    02  Short               i16
-    03  Int                 i32
-    04  Long                i64
-    05  Float               f32
-    06  Double              f64
-    07  ByteArray           Vec<i8>
-    08  String              String
-    09  List                ListTag
-    10  Compound            Map
-    11  IntArray            Vec<i32>
-    12  LongArray           Vec<i64>
-    13  UByte               u8
-    14  UShort              u16
-    15  UInt                u32
-    16  ULong               u64
-    17  Bytes               Vec<u8>
-    18  ShortArray          Vec<i16>
-    19  UShortArray         Vec<u16>
-    20  UIntArray           Vec<u32>
-    21  ULongArray          Vec<u64>
-    22  I128                i128
-    23  U128                u128
-    24  I128Array           Vec<i128>
-    25  U128Array           Vec<u128>
-    26  StringArray         Vec<String>
-}
+include!("table.rs");
 
 impl Tag {
     /// PascalCase title of this TagID.
@@ -153,7 +146,6 @@ impl TagID {
         *self as usize
     }
 }
-
 
 #[cfg(test)]
 mod tests {
