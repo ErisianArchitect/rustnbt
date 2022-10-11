af33e46b0643a8389006631a57d13274603953fd
SevenSolaris <derekforgione@gmail.com>
2022-10-11 06:32:15 -0700

    Added NBTSize impl for ListTag.

--- src/io.rs
+++ src/io.rs
@@ -1,15 +1,9 @@
 // https://wiki.vg/NBT
 
 #[allow(unused)]
-use std::io::{
-    Write, Read,
-    BufWriter, BufReader,
-    Seek, SeekFrom,
-    Cursor,Error,
-};
+use std::io::{BufReader, BufWriter, Cursor, Error, Read, Seek, SeekFrom, Write};
 
 use crate::tag::*;
-
 
 /// Trait that gives the serialization size of various values.
 pub trait NBTSize {
@@ -17,7 +11,9 @@ pub trait NBTSize {
 }
 
 pub trait NBTRead
-where Self: Sized {
+where
+    Self: Sized,
+{
     fn nbt_read<R: Read>(reader: &mut R) -> Result<Self, Error>;
 }
 
@@ -31,31 +27,39 @@ impl NBTSize for String {
     }
 }
 
+impl NBTSize for Vec<String> {
+    fn size_in_bytes(&self) -> usize {
+        self.iter().map(|value| {
+            value.size_in_bytes()
+        })
+        .sum::<usize>() + 4
+    }
+}
+
 impl NBTSize for Map {
     fn size_in_bytes(&self) -> usize {
-        todo!()
+        self.iter().map(|(name, tag)| {
+                name.size_in_bytes() + tag.size_in_bytes() + 1
+            })
+            .sum::<usize>() + 1
     }
 }
 
-impl NBTSize for ListTag {
+impl NBTSize for Vec<Map> {
     fn size_in_bytes(&self) -> usize {
-        todo!()
+        self.iter().map(|value| {
+            value.size_in_bytes()
+        })
+        .sum::<usize>() + 4
     }
 }
 
-pub trait NBTPrimitiveSize {
-    const SIZE: usize;
-    fn primitive_size_in_bytes() -> usize {
-        Self::SIZE
-    }
-}
-
-impl<T> NBTSize for T
-where
-    T: NBTPrimitiveSize,
-{
+impl NBTSize for Vec<ListTag> {
     fn size_in_bytes(&self) -> usize {
-        Self::SIZE
+        self.iter().map(|value| {
+            value.size_in_bytes()
+        })
+        .sum::<usize>() + 4
     }
 }
 
@@ -63,19 +67,22 @@ macro_rules! primitive_table {
     ($($($primitive:ty)+ = $size:literal)+) => {
         $(
             $(
-                impl NBTPrimitiveSize for $primitive {
-                    const SIZE: usize = $size;
+
+                impl NBTSize for $primitive {
+                    fn size_in_bytes(&self) -> usize {
+                        $size
+                    }
                 }
 
                 impl NBTSize for Vec<$primitive> {
                     fn size_in_bytes(&self) -> usize {
-                        4usize + self.len() * <$primitive as NBTPrimitiveSize>::SIZE
+                        self.len() * $size + 4
                     }
                 }
 
                 impl NBTRead for $primitive {
                     fn nbt_read<R: Read>(reader: &mut R) -> Result<Self, Error> {
-                        let mut buf = [0u8; Self::SIZE];
+                        let mut buf = [0u8; $size];
                         reader.read_exact(buf.as_mut_slice())?;
                         Ok(Self::from_be_bytes(buf))
                     }
@@ -99,3 +106,46 @@ primitive_table![
     i64 u64 f64 = 8
     i128 u128 = 16
 ];
+
+macro_rules! tag_info_table {
+    ($($id:literal $title:ident $($type_:ty)?)+) => {
+        impl NBTSize for ListTag {
+            fn size_in_bytes(&self) -> usize {
+                macro_rules! arm_match {
+                    ($tag_title:ident $item_ident:ident $_:ty) => {
+                        ListTag::$tag_title($item_ident)
+                    };
+                    ($tag_title:ident) => {
+                        ListTag::$tag_title
+                    };
+                }
+                macro_rules! arm_result {
+                    ($tag_title:ident $item_ident:ident $_:ty) => {
+                        $item_ident.iter().map(|item| item.size_in_bytes()).sum::<usize>() + 5
+                    };
+                    ($tag_title:ident) => {
+                        5
+                    };
+                }
+                match self {
+                    $(arm_match!($title $(item $type_)?) => arm_result!($title $(item $type_)?),)+
+                }
+            }
+        }
+    };
+}
+
+include!("table.rs");
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+
+    use crate::tag::*;
+    use super::*;
+    #[test]
+    fn size_test() {
+        let tag = Tag::List(ListTag::from(vec![vec![1,2,3],vec![1,2,3],vec![1,2,3]]));
+        assert_eq!(53, tag.size_in_bytes());
+    }
+}
