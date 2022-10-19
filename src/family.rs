1ee61e20285ad41a286e54d1d66fa75f230b2c15
Derek Forgione <derekforgione@gmail.com>
2022-10-19 05:40:32 -0700

    Updated documentation.

--- src/family.rs
+++ src/family.rs
@@ -1,5 +1,9 @@
+// The traits in this file are for marking types for trait bounds.
+/// Marks a type as neither i8 or u8.
 pub trait NonByte {}
+/// Marks a type as a primitive (scalar types such as integers or floating point numbers)
 pub trait Primitive {}
+/// Marks a type as both NonByte and Primitive.
 pub trait NonBytePrimitive {}
 impl<T: NonBytePrimitive> NonByte for T {}
 impl<T: NonBytePrimitive> Primitive for T {}
