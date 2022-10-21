523ad4eabc66e013351304944f962e0e6464d04f
Derek Forgione <derekforgione@gmail.com>
2022-10-20 23:01:53 -0700

    Implemented Display for NamedTag.

--- src/tag.rs
+++ src/tag.rs
@@ -435,6 +435,12 @@ impl Display for ListTag {
     }
 }
 
+impl Display for NamedTag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:#?}", self))
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
