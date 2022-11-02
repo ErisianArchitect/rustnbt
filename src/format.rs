dd858822e2204db5c6019dccd5ed702d86b89275
Derek Forgione <derekforgione@gmail.com>
2022-11-02 14:13:50 -0700

    I'm still planning this, but I wanted to push.

--- src/format.rs
+++ src/format.rs
@@ -3,8 +3,20 @@ The format module is for formatting NBT Tags into SNBT, which is a modified
 version of JSON.
 "]
 
+use std::fmt::Write;
+
 // Requirement: Trait similar to Display that can be applied to NBT types.
 
-trait FormatNbt<T> {
-    fn format_nbt(input: &T) -> String;
+struct NbtFormatter {
+    buffer: String,
+}
+
+impl Write for NbtFormatter {
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+trait FormatNbt: Sized {
+    fn format_nbt<W: Write>(&self, writer: W);
 }
